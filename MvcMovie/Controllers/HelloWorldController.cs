4f31f3942201ccd9e5a2dbc0cb12e60c378d7c20
THANHLUAN\thanhluan <thanhluan@quangich.com>
2018-11-26 11:20:58 +0700

    first commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "Hello world!";
+        //}
+
+        //public string Welcome()
+        //{
+        //    return "Chào mừng...";
+        //}
+
+        ///// <summary>
+        ///// /HelloWorld/Welcome?name=Rick&numtimes=4
+        ///// </summary>
+        ///// <param name="name">Rick</param>
+        ///// <param name="numTimes">4</param>
+        ///// <returns></returns>
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        ///// <summary>
+        ///// /HelloWorld/Welcome/3?name=Rick
+        ///// </summary>
+        ///// <param name="name"></param>
+        ///// <param name="ID"></param>
+        ///// <returns></returns>
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        /// <summary>
+        /// /HelloWorld/Welcome?name=Rick&numtimes=4
+        /// </summary>
+        /// <param name="name">Rick</param>
+        /// <param name="numTimes">4</param>
+        /// <returns></returns>
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]//Đổi tên cột
+        [DataType(DataType.Date)]//Định dạng kiểu dữ liệu lưu trữ
+        public DateTime ReleaseDate { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]//Định dạng kiểu dữ liệu nhập vào
+
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
